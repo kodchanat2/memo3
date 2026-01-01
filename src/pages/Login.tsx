8a925f2c15791ff27b723d4b11d339ea3ce79e83
J0HN_lnwza <kodchanat2@gmail.com>
2026-01-02 01:18:51 +0700

    feat(auth): add login as guest

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,6 @@
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, signInAnonymously } from "firebase/auth";
 import { auth, googleProvider } from "../lib/firebase";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import { Button } from "@/components/ui/button";
 import {
@@ -11,18 +10,26 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 export default function Login() {
   const [error, setError] = useState<string | null>(null);
-  const navigate = useNavigate();
 
   const handleGoogleSignIn = async () => {
     try {
       await signInWithPopup(auth, googleProvider);
-      navigate("/"); // Redirect to home on success
     } catch (err: any) {
       console.error("Login failed", err);
       setError(err.message || "Failed to sign in");
+    }
+  };
+
+  const handleAnonymousSignIn = async () => {
+    try {
+      await signInAnonymously(auth);
+    } catch (err: any) {
+      console.error("Anonymous login failed", err);
+      setError(err.message || "Failed to sign in anonymously");
     }
   };
 
@@ -54,6 +61,22 @@ export default function Login() {
           >
             <Icon icon="logos:google-icon" className="mr-2 h-5 w-5" />
             Sign in with Google
+          </Button>
+
+          <div className="flex items-center gap-2 w-full justify-center">
+            <Separator className="shrink"/>
+            <span className="px-1 text-xs uppercase text-muted-foreground">
+              Or
+            </span>
+            <Separator className="shrink"/>
+          </div>
+
+          <Button
+            variant="secondary"
+            className="w-full"
+            onClick={handleAnonymousSignIn}
+          >
+            Continue as Guest
           </Button>
         </CardContent>
       </Card>
