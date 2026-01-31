fbabbeda4798450359b21b310a35189482cfc39c
J0HN_lnwza <kodchanat2@gmail.com>
2026-01-31 16:06:28 +0700

    feat: loading component

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,17 +1,19 @@
 import { Icon } from "@iconify/react";
 import { useAuth } from "@/hooks/useAuth";
 import Login from "@/pages/Login";
+import AITextLoading from "../kokonutui/ai-text-loading";
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const { user, loading } = useAuth();
 
   if (loading) {
     return (
-      <div className="flex h-screen w-full items-center justify-center bg-background">
+      <div className="flex flex-col h-screen w-full items-center justify-center bg-background">
         <Icon
           icon="line-md:loading-twotone-loop"
           className="size-12 text-foreground"
         />
+        <AITextLoading texts={["Loading...", "Almost there..."]} />
       </div>
     );
   }
