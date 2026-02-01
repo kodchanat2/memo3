e16ee9e000789dfad1715ec3f5d9ddf3a717778e
J0HN_lnwza <kodchanat2@gmail.com>
2026-02-01 22:49:59 +0700

    feat: menu nav on sidebar

--- src/components/section/SideSheet.tsx
+++ src/components/section/SideSheet.tsx
@@ -12,11 +12,19 @@ import { Button } from '@/components/ui/button'
 import { useTheme } from '@/components/ui/theme-provider'
 import { Switch } from '@/components/ui/switch'
 import { Label } from '@/components/ui/label'
+import { Link, useLocation } from 'react-router-dom'
 
 interface SideSheetProps {
   open: boolean
   onOpenChange: (open: boolean) => void
 }
+
+const MENUS = [
+  { icon: 'lucide:settings', label: 'Settings' },
+  { icon: 'lucide:house', label: 'Home', link: '/' },
+  { icon: 'lucide:list-todo', label: 'Todo', link: '/todo' },
+  { icon: 'lucide:info', label: 'About', link: '/about' },
+]
 
 export default function SideSheet({ open, onOpenChange }: SideSheetProps) {
   const { user } = useAuth()
@@ -53,15 +61,11 @@ export default function SideSheet({ open, onOpenChange }: SideSheetProps) {
         {/* Scrollable Menu Section */}
         <div className="flex-1 overflow-y-auto py-2">
           <div className="px-2 space-y-1">
-            <Button
-              variant="ghost"
-              className="w-full justify-start gap-3 hover:bg-background"
-            >
-              <Icon icon="lucide:settings" className="h-5 w-5" />
-              Settings
-            </Button>
+            {MENUS.map((menu) => (
+              <MenuButton key={menu.link} icon={menu.icon} label={menu.label} link={menu.link} onOpenChange={onOpenChange} />
+            ))}
 
-            <div className="flex items-center justify-between px-3 py-2">
+            <div className="flex items-center justify-between px-3 py-2 mt-4">
               <div className="flex items-center gap-3">
                 <Icon
                   icon={theme === 'dark' ? "lucide:moon" : "lucide:sun"}
@@ -94,4 +98,23 @@ export default function SideSheet({ open, onOpenChange }: SideSheetProps) {
       </SheetContent>
     </Sheet>
   )
+}
+
+function MenuButton({ icon, label, link, onOpenChange }: { icon: string, label: string, link?: string, onOpenChange: (open: boolean) => void }) {
+  const location = useLocation()
+  const buttonComp = (
+    <Button
+      variant="ghost"
+      className={`w-full justify-start gap-3 hover:bg-background ${link === location.pathname ? 'bg-background' : ''}`}
+      disabled={link === location.pathname || !link}
+    >
+      <Icon icon={icon} className="h-5 w-5" />
+      {label}
+    </Button>
+  )
+
+  if (link) {
+    return <Link to={link} onClick={() => onOpenChange(false)}>{buttonComp}</Link>
+  }
+  return buttonComp
 }
