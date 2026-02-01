a8cf011b2efe0d842f0ec422d1a8a996bf82516f
J0HN_lnwza <kodchanat2@gmail.com>
2026-02-01 22:32:46 +0700

    feat: loading transition

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -2,25 +2,35 @@ import { Icon } from "@iconify/react";
 import { useAuth } from "@/hooks/useAuth";
 import Login from "@/pages/Login";
 import AITextLoading from "../kokonutui/ai-text-loading";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const { user, loading } = useAuth();
 
-  if (loading) {
-    return (
-      <div className="flex flex-col h-screen w-full items-center justify-center bg-background">
-        <Icon
-          icon="line-md:loading-twotone-loop"
-          className="size-12 text-foreground"
-        />
-        <AITextLoading texts={["Loading...", "Almost there..."]} />
-      </div>
-    );
-  }
+  return (
+    <AnimatePresence mode="wait" >
+      <motion.div
+        key={loading ? "loading" : "content"}
+        initial={{ opacity: 1, scale: 0.98 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, }}
+        transition={{ duration: 0.3 }}
+        className="w-full"
+      >
+        {loading ? <LoadingMask /> : !user ? <Login /> : children}
+      </motion.div>
+    </AnimatePresence>
+  )
+}
 
-  if (!user) {
-    return <Login />
-  }
-
-  return <>{children}</>;
+function LoadingMask() {
+  return (
+    <div className="flex flex-col h-screen w-full items-center justify-center bg-background">
+      <Icon
+        icon="line-md:loading-twotone-loop"
+        className="size-12 text-foreground"
+      />
+      <AITextLoading texts={["Loading...", "Almost there..."]} />
+    </div>
+  );
 }
